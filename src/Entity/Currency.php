78e806f5fffb3fae339bfeab956009ee0ae3ef3b
Krzysztof Sternik <sternik15@gmail.com>
2022-07-06 12:26:39 +0200

    Create Currency entity

---
+++ src/Entity/Currency.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CurrencyRepository;
+use Ramsey\Uuid\Doctrine\UuidGenerator;
+
+#[ORM\Entity(repositoryClass: CurrencyRepository::class)]
+class Currency
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'uuid', unique: true)]
+    #[ORM\GeneratedValue(strategy="CUSTOM")]
+    #[ORM\CustomIdGenerator(class=UuidGenerator::class)]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $currency_code;
+
+    #[ORM\Column(type: 'float')]
+    private $exchange_rate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCurrencyCode(): ?string
+    {
+        return $this->currency_code;
+    }
+
+    public function setCurrencyCode(string $currency_code): self
+    {
+        $this->currency_code = $currency_code;
+
+        return $this;
+    }
+
+    public function getExchangeRate(): ?float
+    {
+        return $this->exchange_rate;
+    }
+
+    public function setExchangeRate(float $exchange_rate): self
+    {
+        $this->exchange_rate = $exchange_rate;
+
+        return $this;
+    }
+}
