03cb47371de5419a8f8784875171f628d0376d18
Krzysztof Sternik <sternik15@gmail.com>
2022-07-06 14:00:33 +0200

    Create index page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CurrencyRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(CurrencyRepository $currencyRepo): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'title' => 'Currency Exchange Rates',
+            'rates' => $currencyRepo->findAll()
+        ]);
+    }
+}
